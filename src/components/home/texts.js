225aeb2ccdc2f4940a3a9955548203a5bd68e83f
Nikola Krivokapic <nikola@paperpile.com>
2020-03-13 12:32:36 +0100

    Updates

--- src/components/home/texts.js
+++ src/components/home/texts.js
@@ -1,11 +1,11 @@
 export default {
-  text1: `I am software developer, mainly focused on full stack web development. 
-     I have around 8 years of professional experience, working mostly remotely as part of the agile teams. 
-     Currently, I am owner of legal entity "Bokasoft" under which I work as a contractor for various renowned international clients.`,
+  text1: `I am software developer, mainly focused on full stack web development,
+     with around 8 years of professional experience, working mostly remotely as part of the agile teams. 
+     Currently, I work as a contractor for various renowned international clients under legal entity "Bokasoft"`,
   text2: `, I have been working on browser simulations for various academic institutions 
      like University of New South Wales, University of Sydney, University of Arizona and many others in the fields like Math, Chemistry, 
      Biology & Statistics & Astrology.`,
-  text3: `I have extensive experience working with many modern web technologies on both backend and frontend with high coding 
+  text3: `I am experienced working with many modern web technologies on both backend and frontend with high coding 
     standards achieving high-end standardized final products. Some of those technologies are PHP, MySql, Laravel, Symfony, Yii2, MongoDB, 
     Reddis, Javascript, ES5-ES6-ES7, Backbone, Jquery, Mocha, Chai, React, Redux, Redux-Thunk, RxJS, Redux-Observables, Enzyme, 
     Node.js, Express, C3, D3, Three JS, Underscore, Lodash, CSS, Less, Sass, Styled-components. I also worked on a few Android - 
